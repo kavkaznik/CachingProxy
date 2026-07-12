9404b45c8efd6d74dd0c8b6af2aceb2cf58bb31c
aalibay <akejana393@gmail.com>
2026-07-12 19:01:14 +0400

    scratch

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+)
+
+var port int
+var url string
+
+func main() {
+	flag.IntVar(&port, "port", 0, "port for url")
+	flag.StringVar(&url, "origin", "localhost", "request address")
+	flag.Parse()
+
+	http.HandleFunc("/", mo)
+
+	err := http.ListenAndServe("localhost:8081", nil)
+	log.Fatal(err)
+}
+
+func mo(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get(url + r.URL.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(body)
+}
